cf647d49bb9b998535ffbfb56a512334f9b0e111
Lua <22667586+luaxi@users.noreply.github.com>
2026-01-27 23:33:32 -0300

    First commit

---
+++ src/main/java/com/example/estudio_api/EstudioApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.estudio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/estudio_api/EstudioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estudio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
