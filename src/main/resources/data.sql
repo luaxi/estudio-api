90e424184169dacb0ca4e2f5544f89212b5ac163
Lua <22667586+luaxi@users.noreply.github.com>
2026-01-28 14:17:03 -0300

    Adicionada base de dados para demonstração

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO clientes (nome, telefone) VALUES
+  ('Banda Dejavu', '51939393933'),
+  ('Maria Cantora', '51949494944');
+
+INSERT INTO salas (nome, preco_por_hora) VALUES
+  ('Sala A', 50.00),
+  ('Sala B', 30.00),
+  ('Sala C', 15.00);
+
+INSERT INTO equipamentos (nome, tipo, sala_id) VALUES
+  ('Fender Stratocaster 1961 Rocky', 'GUITARRA', 1),
+  ('DW Collectors Neil Peart R30 Tribute', 'BATERIA', 1),
+  ('Höfner 500/1', 'BAIXO', 2),
+  ('Casio CTK-3500', 'TECLADO', 2),
+  ('Shure SM7B', 'MICROFONE', 3),
+  ('Fender Mustangs', 'GUITARRA', 3);
